8fd7e180b2fe5643def5b215723dc7d00bf27190
sachabarbet <sacha.barbet@proton.me>
2025-04-22 10:46:26 +0200

    Init spring boot rest api

---
+++ src/main/java/io/github/pause_clope/PauseClopeApplication.java
@@ -0,0 +1,13 @@
+package io.github.pause_clope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PauseClopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PauseClopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pause_clope/PauseClopeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pause_clope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PauseClopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
